525e0eae611b66a766065c9c554c199c2dc2f2dc
leticiagomes <dev.leticia.nascimento@gmail.com>
2026-08-05 14:36:23 -0300

    criação da pagina mongólia

---
+++ script/mongolia.js
@@ -0,0 +1,41 @@
+const btnHistoria = document.getElementById("btnHistoria");
+const btnTurismo = document.getElementById("btnTurismo");
+
+const modalHistoria = document.getElementById("modalHistoria");
+const modalTurismo = document.getElementById("modalTurismo");
+
+const fecharHistoria = document.getElementById("fecharHistoria");
+const fecharTurismo = document.getElementById("fecharTurismo");
+
+// Abrir modal História
+btnHistoria.onclick = function () {
+    modalHistoria.style.display = "flex";
+};
+
+// Abrir modal Turismo
+btnTurismo.onclick = function () {
+    modalTurismo.style.display = "flex";
+};
+
+// Fechar modal História
+fecharHistoria.onclick = function () {
+    modalHistoria.style.display = "none";
+};
+
+// Fechar modal Turismo
+fecharTurismo.onclick = function () {
+    modalTurismo.style.display = "none";
+};
+
+// Fechar clicando fora do modal
+window.onclick = function(event){
+
+    if(event.target == modalHistoria){
+        modalHistoria.style.display = "none";
+    }
+
+    if(event.target == modalTurismo){
+        modalTurismo.style.display = "none";
+    }
+
+};
